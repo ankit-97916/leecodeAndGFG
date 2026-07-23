9c57b4d8c2fab90a9510e356bb83f39ea5d6b089
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-23 13:15:24 +0530

    Time: 10 ms (5.69%), Space: 51.2 MB (92.2%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        boolean[][] visit = new boolean[rows][cols];
+        Queue<int[]> q = new LinkedList<>();
+        int island = 0;
+        // check all cells;
+        for(int i=0; i<rows; i++){
+            for(int j=0; j<cols; j++){
+                if(grid[i][j] == '1' && !visit[i][j]){
+                    island++;
+                    // start bfs = ;
+                    visit[i][j] = true;
+                    q.add(new int[]{i,j});
+                    while(!q.isEmpty()){
+                        // remove row and col extract--
+                        int[] remove = q.poll();
+                        int row = remove[0];
+                        int col = remove[1];
+
+                        // add up 
+                        if(row-1 >= 0 && grid[row-1][col] == '1' && !visit[row-1][col]){
+                            visit[row-1][col] = true;
+                            q.add(new int[]{row-1,col});
+                        }
+
+                         // add down 
+                        if(row+1 <rows && grid[row+1][col] == '1' && !visit[row+1][col]){
+                            visit[row+1][col] = true;
+                            q.add(new int[]{row+1,col});
+                        }
+
+                         // add left 
+                        if(col-1 >= 0 && grid[row][col-1] == '1' && !visit[row][col-1]){
+                            visit[row][col-1] = true;
+                            q.add(new int[]{row,col-1});
+                        }
+
+                        //  // add right----- 
+                        if(col+1 <cols && grid[row][col+1] == '1' && !visit[row][col+1]){
+                            visit[row][col+1] = true;
+                            q.add(new int[]{row,col+1});
+                        }
+                    }
+                }
+            }
+        }
+        // return 
+        return island;
+        
+    }
+}
