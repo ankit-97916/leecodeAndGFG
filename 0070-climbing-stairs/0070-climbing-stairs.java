0af12a4cdc068e7a5db0a2421f4ef93bdfc95f2d
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-25 22:55:19 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -5,16 +5,18 @@ class Solution {
     }
 
  public static int fibo( int n){
-    int[] dp = new int[n+1]; 
+    // int[] dp = new int[n+1]; 
   if(n <= 2){
     return n;
   }
   //
-  dp[0] =1;
-  dp[1] =1;
-  for(int i=2; i<dp.length; i++){
-    dp[i] = dp[i-1]+dp[i-2];
+  int prev1 =1;
+  int prev2 =1;
+  for(int i=2; i<=n; i++){
+   int c= prev1+prev2;
+   prev1 = prev2;
+   prev2 = c;
   }
-  return dp[n];
+  return prev2;
 }
 }
