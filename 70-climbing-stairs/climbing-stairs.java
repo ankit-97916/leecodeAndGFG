11fee89c090696315a15039827aade603a02101a
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-25 22:52:42 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (36.16%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,20 @@
 class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
-        return fibo(n, dp); 
+        return fibo(n); 
     }
 
- public static int fibo( int n , int[] dp){
+ public static int fibo( int n){
+    int[] dp = new int[n+1]; 
   if(n <= 2){
     return n;
   }
   //
-  if(dp[n] != 0){
-    return dp[n];
+  dp[0] =1;
+  dp[1] =1;
+  for(int i=2; i<dp.length; i++){
+    dp[i] = dp[i-1]+dp[i-2];
   }
-
-  int x = fibo(n-1 , dp);
-  int y = fibo(n-2, dp);
-  return dp[n] =  x+y;
+  return dp[n];
 }
 }
