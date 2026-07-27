36d066a6d3d0e28de8b2886ffc469dfdd16786bc
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-27 11:54:27 +0530

    Time: 0 ms (100%), Space: 43 MB (19.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,27 +1,32 @@
 class Solution {
     public int rob(int[] arr) {
-        int dp[]  = new int[arr.length];
+        // int dp[]  = new int[arr.length];
         // for(int i=0; i<dp.length; i++){
         //     dp[i] = -1;
         // }
-        Arrays.fill(dp, -1);
-        return robhouse(arr, arr.length-1, dp);
+        // Arrays.fill(dp, -1);
+        return robhouse(arr);
         
     }
-    public static int robhouse(int[] arr , int i, int[] dp){
-        if(i  < 0){
-            return 0;
+    public static int robhouse(int[] arr){
+        if(arr.length == 1){
+            return arr[0];
         }
         //
-        if(dp[i] !=  -1){
-            return dp[i];
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+        //
+        for(int i=2; i<dp.length; i++){
+            int rob = arr[i] + dp[i-2];
+            int dontrob = dp[i-1];
+             dp[i] = Math.max(rob, dontrob);
         }
 
+        return dp[dp.length-1];
+       
         
-      int rob = arr[i] + robhouse(arr, i-2, dp);
-      int dontrob =  robhouse(arr, i-1, dp);
-
-    return dp[i] =  Math.max(rob, dontrob);
+     
 
 
 
