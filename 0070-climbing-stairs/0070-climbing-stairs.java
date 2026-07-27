fd87b64e8c7fa9ccff0d029bcd4140f9ab7198d5
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-27 12:55:57 +0530

    Time: 0 ms (100%), Space: 42.1 MB (55.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -9,12 +9,12 @@ class Solution {
             return n;
         }
         int[] dp = new int[n+1];
-        dp[0] =1 ;
-        dp[1] = 1;
-        for(int i=2; i<dp.length; i++){
-            dp[i] = dp[i-1]+dp[i-2];
+        dp[dp.length-1] =1 ;
+        dp[dp.length-2] = 1;
+        for(int i=dp.length-3; i>=0; i--){
+            dp[i] = dp[i+1]+dp[i+2];
         }
-        return dp[n];
+        return dp[0];
     
        
     }
