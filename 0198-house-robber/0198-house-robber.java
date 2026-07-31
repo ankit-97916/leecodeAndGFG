7fc726c1b808b48979faf0ea3a7323d15548d686
Ankit Kumar <kumarankittdl31@gmail.com>
2026-07-31 12:36:38 +0530

    Time: 0 ms (100%), Space: 42.7 MB (57.47%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,22 +1,27 @@
 class Solution {
     public int rob(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp, -1);
-        return chor(arr, n-1, dp);
+        // int n = arr.length;
+        // int[] dp = new int[n];
+        // Arrays.fill(dp, -1);
+        return chor(arr);
         
     }
-    public static int chor(int[] arr, int i , int[] dp){
-        if(i < 0){
-            return 0;
+    public static int chor(int[] arr){
+        if(arr.length == 1){
+            return arr[0];
         }
-        // apply dp--
-        if(dp[i] != -1){
-            return dp[i];
-        }
-        int add = arr[i] + chor(arr, i-2, dp);
-        int not = chor(arr, i-1, dp);
+       int[] dp = new int[arr.length];
 
-        return  dp[i] =  Math.max(add, not);// remembber dp --
+       dp[0] =  arr[0];
+       dp[1] =  Math.max(arr[0], arr[1]);
+       ////
+       for(int i=2; i<dp.length; i++){
+       int rob = arr[i] + dp[i-2];
+       int dont = dp[i-1];
+
+       dp[i] =  Math.max(rob, dont);
+       }
+       return dp[arr.length-1];
+
     }
 }
