8ec727110173a6fade85d4dd35c286d566d3a9a1
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-01 16:32:17 +0530

    Time: 1 ms (99.63%), Space: 47 MB (79.6%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,37 @@
 class Solution {
-    public int minFallingPathSum(int[][] matrix) {
+    public int minFallingPathSum(int[][] grid) {
+        int[][] dp = new int[grid.length][grid[0].length];
+        for(int[] a : dp){
+            Arrays.fill(a, -999999);
+        }
+        int ans = Integer.MAX_VALUE;
 
-      int[][] dp = new int[matrix.length][matrix[0].length];
-			for (int[] a : dp) {
-				Arrays.fill(a, -100000);
-			}
-			int ans = Integer.MAX_VALUE;
-			for (int i = 0; i < matrix[0].length; i++) {
-				ans = Math.min(ans, Falling_Path_Sum(matrix, 0, i, dp));
-			}
-			return ans;
-		}
+        for(int j= 0; j<grid[0].length; j++){
+            ans = Math.min(ans, minFall(grid, 0, j, dp ));
+        }
 
-		public static int Falling_Path_Sum(int[][] matrix, int cr, int cc, int[][] dp) {
-			if (cc < 0 || cc >= matrix[0].length) {
-				return Integer.MAX_VALUE;
-			}
-			if (cr == matrix.length - 1) {
-				return matrix[cr][cc];
-			}
-			if (dp[cr][cc] != -100000) {
-				return dp[cr][cc];
-			}
-			int ld = Falling_Path_Sum(matrix, cr + 1, cc - 1, dp);
-			int down = Falling_Path_Sum(matrix, cr + 1, cc, dp);
-			int rd = Falling_Path_Sum(matrix, cr + 1, cc + 1, dp);
-			return dp[cr][cc] = Math.min(down, Math.min(ld, rd)) + matrix[cr][cc];
+        return ans;
+    }
+    public static int minFall(int[][] grid, int cr, int cc, int[][] dp){
+        //base case out of range--
 
-		}
+        if(cc < 0 || cr >= grid.length || cc >= grid[0].length){
+            return Integer.MAX_VALUE;
+        }
+        // base case destination --
+        if(cr == grid.length-1){
+            return grid[cr][cc];
+        }
+            // apply dp
+            if(dp[cr][cc] != -999999){
+                return dp[cr][cc];
+            }
+        // choices--
+        int left = minFall(grid, cr+1, cc-1, dp);
+        int right = minFall(grid, cr+1, cc+1, dp);
+        int down = minFall(grid, cr+1, cc, dp);
+
+        return dp[cr][cc] =  Math.min(left, Math.min(right, down) ) +  grid[cr][cc];
+
+    }
 }
