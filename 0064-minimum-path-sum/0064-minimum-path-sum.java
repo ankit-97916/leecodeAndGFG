21081cb9efb44bc687d581b4d60c79b88a71cd21
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-01 15:39:02 +0530

    Time: 1 ms (99.92%), Space: 50.4 MB (39.1%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.java
+++ 0064-minimum-path-sum/0064-minimum-path-sum.java
@@ -1,29 +1,31 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-      
-      int[][] dp = new int[grid.length][grid[0].length];
-
+        int[][] dp = new int[grid.length][grid[0].length];
         for(int[] a : dp){
-            Arrays.fill(a,-1);
+            Arrays.fill(a, -1);
         }
-        return pathSum(grid, 0,0,dp);
+        return minPath(grid, 0 , 0, dp);
     }
 
-    public static int pathSum(int [][] grid, int cc, int cr, int[][] dp){
-       if(cr == grid.length-1 && cc == grid[0].length-1){
-        return  grid[cr][cc];
-       }
-
-       if(cr >= grid.length || cc >= grid[0].length){
-        return Integer.MAX_VALUE;
-       }
+    public static int minPath(int[][] grid, int cr, int cc, int[][] dp){
+        // base case -- reach destination --
+        if(cr == grid.length-1 && cc == grid[0].length-1){
+            return grid[cr][cc];
+        }
+        // base case-- range k bhar 
+        if(cr >= grid.length || cc >= grid[0].length){
+            return Integer.MAX_VALUE;
+        }
 
         if(dp[cr][cc] != -1){
             return dp[cr][cc];
         }
-        int right =  pathSum(grid, cc, cr+1, dp);
-        int down =  pathSum(grid, cc+1, cr, dp);
-        return dp[cr][cc] =  Math.min(right, down) + grid[cr][cc];
+         //increase rows
+        int down =  minPath(grid, cr+1, cc, dp );
+        //increae cols
+        int right =  minPath(grid, cr, cc+1 , dp);
+
+        return dp[cr][cc] =  grid[cr][cc] + Math.min(right, down);
 
     }
 }
