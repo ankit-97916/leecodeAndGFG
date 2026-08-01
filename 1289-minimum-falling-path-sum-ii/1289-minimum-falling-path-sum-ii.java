a7c90adff5726d4f39bcb604ae4f297ace726f7d
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-01 16:44:13 +0530

    Time: 172 ms (35.36%), Space: 55 MB (23.67%) - LeetHub

--- 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.java
@@ -1,30 +1,36 @@
 class Solution {
-    public int minFallingPathSum(int[][] grid) {
-      int[][] dp = new int[grid.length][grid[0].length];
-		for (int[] a : dp) {
-			Arrays.fill(a, -100000);
-		}
-		int ans = Integer.MAX_VALUE;
-		for (int i = 0; i < grid[0].length; i++) {
-			ans = Math.min(ans, Falling_Path_Sum(grid, 0, i, dp));
-		}
-		return ans;
-	}
+    public int minFallingPathSum(int[][] matrix) {
 
-	public static int Falling_Path_Sum(int[][] grid, int cr, int cc, int[][] dp) {
-		if (cr == grid.length - 1) {
-			return grid[cr][cc];
+        int ans = Integer.MAX_VALUE;
+		int[][] dp = new int[matrix.length][matrix[0].length];
+		for (int[] a : dp) {
+			Arrays.fill(a, -999999);
 		}
-		if (dp[cr][cc] != -100000) {
+		for (int i = 0; i < matrix[0].length; i++) {
+			ans = Math.min(ans, Falling_Path_Sum(matrix, 0, i, dp));
+		}
+
+        return ans;
+        
+    }
+
+    private static int Falling_Path_Sum(int[][] matrix, int cr, int cc, int[][] dp) {
+		// TODO Auto-generated method stub
+		if (cr == matrix.length - 1) {
+			return matrix[cr][cc];
+		}
+		if(dp[cr][cc]!= -999999) {
 			return dp[cr][cc];
 		}
 		int ans = Integer.MAX_VALUE;
-		for (int col = 0; col < dp.length; col++) {
-			if (col != cc) {
-				ans = Math.min(ans, Falling_Path_Sum(grid, cr + 1, col, dp));
+		for (int col = 0; col < matrix.length; col++) {
+			if (col == cc) {
+				continue;
 			}
-		}
-		return dp[cr][cc]=ans+grid[cr][cc];
 
+            ans = Math.min(ans, Falling_Path_Sum(matrix, cr + 1, col, dp));
+
+		}
+		return dp[cr][cc] = ans + matrix[cr][cc];
 	}
 }
