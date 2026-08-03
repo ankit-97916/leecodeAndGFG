3ef0bd09fa98013169f463fa6ea5bb93dc9a9eb3
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-03 10:01:10 +0530

    Time: 1 ms (99.95%), Space: 46.7 MB (93.14%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int singleNumber(int[] arr) {
+       //bitwise operatr-
+       int xor = 0;
+       for(int i=0; i<arr.length; i++){
+         xor = xor ^ arr[i];
+       }
+       return xor;
+        
+    }
+}
