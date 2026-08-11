310a5db7a08f116f8dc0bb1a1321c04205469b58
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-11 16:39:26 +0530

    Time: 39 ms (5.03%), Space: 110.6 MB (7.4%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,22 +1,17 @@
 class Solution {
     public boolean containsDuplicate(int[] arr) {
-    HashMap<Integer, Integer> map = new HashMap<>();
-    //frequncy count
-    for(int i=0; i<arr.length; i++){
-        if(map.containsKey(arr[i])){
-            map.put(arr[i], map.get(arr[i])+ 1);
-    }else{
-        map.put(arr[i], 1);
-    }
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i<arr.length; i++){
+            map.put(arr[i], map.getOrDefault(arr[i],0)+1);
+        }
 
-    }
+        for(int i=0; i<arr.length;i++){
+            if(map.get(arr[i]) > 1){
+                return true;
+            }
+        }
 
-    for(int i=0; i<arr.length; i++){
-          if(map.get(arr[i]) >1){
-            return true;
-          }
-    }
+        return false;
 
-    return false;
     }
 }
