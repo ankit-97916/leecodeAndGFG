7d791940a328975daa1845ea517a790ca715244d
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-11 17:56:57 +0530

    Time: 4 ms (29.25%), Space: 45 MB (56.27%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,34 +1,36 @@
 class Solution {
-    public int[] intersection(int[] arr1, int[] arr2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        Set<Integer> ll = new HashSet<>();
+    public int[] intersection(int[] nums1, int[] nums2) {
+       return  intersectionArray(nums1, nums2);
+    }
 
-        for(int i=0; i<arr1.length; i++){
-            if(map.containsKey(arr1[i])){
-                map.put(arr1[i], map.get(arr1[i]) + 1);
-            }else{
-                map.put(arr1[i], 1);
-            }
+    public static int[] intersectionArray(int[] arr1, int[]arr2){
+        HashMap<Integer, Integer> map =  new HashMap<>();
+        ArrayList<Integer> list =  new ArrayList<>();
+        
+        //store array 1 element in map --- 
+
+        for(int i = 0; i < arr1.length; i++){
+            map.put(arr1[i], map.getOrDefault(arr1[i], 0)+1);
         }
         //
-        for(int i=0; i<arr2.length; i++){
-            if(map.containsKey(arr2[i]) && map.get(arr2[i]) > 0){
-                
-                    ll.add(arr2[i]);
-                
-                map.put(arr2[i] , map.get(arr2[i]) -1);
+        for(int i = 0; i< arr2.length; i++){
+            if(map.containsKey(arr2[i])  && map.get(arr2[i]) > 0){
+               if(!list.contains(arr2[i])){
+                 list.add(arr2[i]);
+               }
+                 map.put(arr2[i], map.get(arr2[i])-1);
             }
+
+           
         }
-        //
-        int[] ans = new int[ll.size()];
-        int i=0;
-        for(Integer num : ll){
-            ans[i] = num;
-            i++;
+
+        int[] ans = new int[list.size()] ;
+        for(int i = 0; i<list.size(); i++){
+            ans[i] =  list.get(i);
         }
 
         return ans;
-        
-        
+
+
     }
 }
