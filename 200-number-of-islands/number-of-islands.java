55888efc28c612e5ffd3ad288c2f3efa6b4db43b
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-22 23:10:02 +0530

    Time: 12 ms (5.33%) | Memory: 51.8 MB (76.90%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,54 +1,56 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int rows = grid.length;
-        int cols = grid[0].length;
+        int rows =  grid.length;
+        int  cols  = grid[0].length;
+        Stack<Pair> st = new Stack<>();
+        boolean visited[][] = new boolean[rows][cols];
+        int islands = 0;
+        for(int i=0; i<rows;i++){
+            for(int j= 0; j<cols; j++){
+                if(grid[i][j] == '1' && !visited[i][j]){
+                   islands++;
 
-        boolean[][] visit = new boolean[rows][cols];
-        Stack<int[]> q = new Stack<>();
-        int island = 0;
-        // check all cells;
-        for(int i=0; i<rows; i++){
-            for(int j=0; j<cols; j++){
-                if(grid[i][j] == '1' && !visit[i][j]){
-                    island++;
-                    // start bfs = ;
-                    visit[i][j] = true;
-                    q.push(new int[]{i,j});
-                    while(!q.isEmpty()){
-                        // remove row and col extract--
-                        int[] remove = q.pop();
-                        int row = remove[0];
-                        int col = remove[1];
-
-                        // add up 
-                        if(row-1 >= 0 && grid[row-1][col] == '1' && !visit[row-1][col]){
-                            visit[row-1][col] = true;
-                            q.push(new int[]{row-1,col});
-                        }
-
-                         // add down 
-                        if(row+1 <rows && grid[row+1][col] == '1' && !visit[row+1][col]){
-                            visit[row+1][col] = true;
-                            q.push(new int[]{row+1,col});
-                        }
-
-                         // add left 
-                        if(col-1 >= 0 && grid[row][col-1] == '1' && !visit[row][col-1]){
-                            visit[row][col-1] = true;
-                            q.push(new int[]{row,col-1});
-                        }
-
-                        //  // add right----- 
-                        if(col+1 <cols && grid[row][col+1] == '1' && !visit[row][col+1]){
-                            visit[row][col+1] = true;
-                            q.push(new int[]{row,col+1});
-                        }
+                   st.push(new Pair(i, j));
+                   while(!st.isEmpty()){
+                    //remove--
+                    Pair remove =  st.pop();
+                    int row = remove.row;
+                    int col =  remove.col;
+                    //ignore--
+                    if(visited[row][col]){
+                        continue;
                     }
+                    //  mark --
+                    visited[row][col] = true;
+
+                    // addunvisited--------
+                    if(row-1 >= 0 && grid[row-1][col] ==  '1' && !visited[row-1][col]){
+                        st.push(new Pair(row-1, col));
+                    }
+
+                      if(row+1 < rows && grid[row+1][col] ==  '1' && !visited[row+1][col]){
+                        st.push(new Pair(row+1, col));
+                    }
+  if(col-1 >= 0 && grid[row][col-1] ==  '1' && !visited[row][col-1]){
+                        st.push(new Pair(row, col-1));
+                    }
+  if(col + 1 < cols && grid[row][col+1] ==  '1' && !visited[row][col+1]){
+                        st.push(new Pair(row, col+1));
+                    }
+
+
+                   }
                 }
             }
         }
-        // return 
-        return island;
-        
+        return islands;
+    }
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row =  row;
+            this.col = col;
+        }
     }
 }
