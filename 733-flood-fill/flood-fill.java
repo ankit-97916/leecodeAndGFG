f9cfea47eb47c7d0ce3fed167cdda8b69d4d6dbe
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-24 12:11:28 +0530

    Time: 2 ms (11.46%) | Memory: 47.1 MB (11.16%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,53 @@
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row, int col){
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+    int rows = image.length;
+    int cols = image[0].length;
+    int oldColor = image[sr][sc];// staring index is oldcolor ya original color ---
+     if(oldColor == color){
+            return image;
+        }
+    Stack<Pair> st = new Stack<>();
+    st.push(new Pair(sr, sc));
+    while(!st.isEmpty()){
+        //remove----
+        Pair remove = st.pop();
+        int row = remove.row;
+        int col =  remove.col;
+
+        //change color and visit mark ek sath-- depend on questions---
+        image[row][col] = color;
+
+        // add valid neighbour in stack ----
+        if(row-1 >= 0 && image[row-1][col] == oldColor){
+            image[row-1][col] = color;
+            st.push(new Pair(row-1, col));
+        }
+         if(row+1 < rows && image[row+1][col] == oldColor){
+            image[row+1][col] = color;
+            st.push(new Pair(row+1, col));
+        }
+         if(col-1 >= 0 && image[row][col-1] == oldColor){
+            image[row][col-1] = color;
+            st.push(new Pair(row, col-1));
+        }
+         if(col+1 < cols && image[row][col+1] == oldColor){
+            image[row][col+1] = color;
+            st.push(new Pair(row, col+1));
+        }
+
+    }
+    return image;
+    
+    }
+    
+    //
+   
+}
