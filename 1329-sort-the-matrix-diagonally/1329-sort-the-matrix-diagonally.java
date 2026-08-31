358bbbccb7f6674a9074e53a10f32c772379ad56
Ankit Kumar <kumarankittdl31@gmail.com>
2026-08-31 12:07:52 +0530

    Time: 6 ms (65.9%), Space: 47.1 MB (12.89%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.java
@@ -1,48 +1,45 @@
 class Solution {
     public int[][] diagonalSort(int[][] mat) {
-       int m = mat.length;
-       int n  =  mat[0].length;
-       for(int j=0; j<n; j++){
-        sortOneDiagonal(mat, 0, j );
-       }
-       for(int i=1; i<m; i++){
-        sortOneDiagonal(mat, i , 0);
-       }
-       return mat;
-    }
+        for(int j=0 ; j< mat[0].length; j++){
+            sortOneDiagonal(mat, 0, j);
+        }
 
-    public static void sortOneDiagonal(int[][] matrix, int row, int col){
-        int m =  matrix.length;
-        int n =  matrix[0].length;
-        // length of diagonal 
-        int length = 0;
+        for(int i=1 ; i< mat.length; i++){
+            sortOneDiagonal(mat, i, 0);
+        }
+        return mat;
+    }
+    public static void sortOneDiagonal(int[][] mat , int   row, int col ){
+        int m = mat.length;
+        int n = mat[0].length;
         int i =row;
-        int j =  col;
+        int j= col;
+        int length = 0;
+        // length---
         while(i < m && j < n){
             length++;
             i++;
             j++;
         }
 
-        // make diagonal size array to chage sort the  alue 
-        int[] diagonal =  new int[length];
-         i = row;
-       int index = 0;
-         j = col;
+        int[]  diagonal = new int[length];
+
+        i=row;
+        j = col;
+        int index = 0;
         while(i < m && j < n){
-            diagonal[index] = matrix[i][j];
-            index++;
-            i++;
-            j++;
+       diagonal[index] = mat[i][j];
+       index++;
+       i++;
+       j++;
         }
-        // sort the diagonal array ---
-        Arrays.sort(diagonal);
-        // put back balues in matrix ---
-         i = row;
-         j  = col;
-         index  = 0;
-        while(i < m && j < n){
-            matrix[i][j] =  diagonal[index];
+       // sort the diagonal
+       Arrays.sort(diagonal);
+        i = row;
+        j =  col;
+        index  =0;
+        while(i < m && j <  n){
+            mat[i][j] =   diagonal[index];
             index++;
             i++;
             j++;
