dbbb8b0e3fdd3b73f45898b4fdf1a3ff68efeee9
Ankit Kumar <kumarankittdl31@gmail.com>
2026-09-15 12:10:28 +0530

    Time: 1 ms (87.97%), Space: 44.5 MB (99.85%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,40 +1,31 @@
 class Solution {
     public List<List<Integer>> permute(int[] arr) {
-
-                
-       List<List<Integer>> result = new ArrayList<>();
-        printPermutaionOfArray(arr, 0, result);
+        List<List<Integer>> result  = new ArrayList<>();
+        List<Integer>  li = new ArrayList<>();
+        arrangment(arr, 0, result , li);
         return result;
 
-        
     }
 
-        public static void printPermutaionOfArray(int[] arr, int idx ,  List<List<Integer>> result){
-      if(idx == arr.length){
-       List<Integer> temp = new ArrayList<>();
-       for(int num : arr){
-        temp.add(num);
-       }
-       result.add(temp);
-        
-        return;
-      }
-      // take curent elemnt
-      for(int i=idx; i<arr.length; i++){
-        // swap
-        swap(arr, i, idx);
-        //rc
-        printPermutaionOfArray(arr, idx+1 ,result);
-        //undo bt
-        swap(arr, i, idx);
-      }
-    }
-  
-       // swa the elmnt -
-    public static void swap(int[] arr , int i, int j){
-      int temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
+    public static void arrangment(int[] arr, int idx,  List<List<Integer>> result ,List<Integer>  li ){
+          if(idx == arr.length){
+            result.add(new ArrayList<>(li));
+            return;
+          }
+
+          for(int i = idx; i < arr.length; i++){
+            swap(arr, idx, i);
+            li.add(arr[idx]);
+           arrangment(arr, idx+1 , result , li);
+           li.remove(li.size()-1);
+           swap(arr, idx , i);
+          }
     }
 
+    // swap fn ---
+    public static void swap(int[] arr,  int i , int j ){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp ;
+    }
 }
