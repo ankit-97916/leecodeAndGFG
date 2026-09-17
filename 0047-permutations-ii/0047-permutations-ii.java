ca8c679dc5226eae3363e338a3895078ce42e712
Ankit Kumar <kumarankittdl31@gmail.com>
2026-09-17 11:55:43 +0530

    Time: 4 ms (29.01%), Space: 47.5 MB (5.77%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.java
+++ 0047-permutations-ii/0047-permutations-ii.java
@@ -1,59 +1,33 @@
 class Solution {
-
-    public List<List<Integer>> permuteUnique(int[] nums) {
-
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> li = new ArrayList<>();
-
-        permutation(nums, 0, li, ans);
-
-        return ans;
+    public List<List<Integer>> permuteUnique(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> result  = new ArrayList<>();
+        List<Integer>  li = new ArrayList<>();
+        arrangment(arr, 0, result , li);
+        return result;
     }
 
-    public void permutation(int[] nums, int i,
-                            List<Integer> li,
-                            List<List<Integer>> ans) {
-
-        // Base case
-        if (i == nums.length) {
-            ans.add(new ArrayList<>(li));
+    public static void arrangment(int[] arr, int idx,  List<List<Integer>> result ,List<Integer>  li ){
+          if(idx == arr.length){
+            result.add(new ArrayList<>(li));
             return;
-        }
-
-        // Is level par kaunse elements use ho chuke hain
-        HashSet<Integer> set = new HashSet<>();
-
-        for (int j = i; j < nums.length; j++) {
-
-            // Agar same element is level par already use ho chuka hai
-            if (set.contains(nums[j])) {
+          }
+      HashSet<Integer> set = new HashSet<>();
+          for(int i = idx; i < arr.length; i++){
+            if(set.contains(arr[i])){
                 continue;
             }
-
-            // Element ko mark kar do
-            set.add(nums[j]);
-
-            // Swap
-            swap(nums, i, j);
-
-            // Add current element
-            li.add(nums[i]);
-
-            // Recursive call
-            permutation(nums, i + 1, li, ans);
-
-            // Backtrack
-            li.remove(li.size() - 1);
-
-            // Undo swap
-            swap(nums, i, j);
-        }
+            set.add(arr[i]);
+            swap(arr, idx, i);
+            li.add(arr[idx]);
+           arrangment(arr, idx+1 , result , li);
+           li.remove(li.size()-1);
+           swap(arr, idx , i);
+          }
     }
-
-    public void swap(int[] nums, int i, int j) {
-
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
+          public static void swap(int[] arr,  int i , int j ){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp ;
     }
 }
