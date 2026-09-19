5867a98bb29e1158957198cf40a5d88c5d2c478c
Ankit Kumar <kumarankittdl31@gmail.com>
2026-09-20 00:14:38 +0530

    Time: 124 ms (90.99%), Space: 43.2 MB (28.49%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,34 +1,35 @@
 class Solution {
     public boolean exist(char[][] arr, String word) {
          for(int i=0; i<arr.length; i++){
-            for(int j=0; j<arr[0].length; j++){
+            for(int j =0; j<arr[0].length; j++){
                 if(arr[i][j] == word.charAt(0)){
-                    boolean ans = isExist(arr, i,j,word, 0);
+                    boolean ans = exit(arr, i,j, word, 0);
                     if(ans){
                         return true;
-
                     }
                 }
             }
          }
          return false;
     }
-    public static boolean isExist(char[][] arr, int cr , int cc, String word, int idx){
-        if(idx == word.length()){
+
+    public static boolean exit(char[][] arr , int cr, int cc, String word , int i){
+        int n = arr.length; 
+        int m = arr[0].length;
+        if( i == word.length()){
             return true;
         }
-
-        if(cr < 0 || cc < 0 || cr >= arr.length || cc >= arr[0].length || arr[cr][cc] != word.charAt(idx)){
+        if(cr < 0 || cc <  0 || cr >=n || cc >= m || arr[cr][cc] != word.charAt(i) ){
             return false;
         }
-            arr[cr][cc] = '*';
-        boolean found = isExist(arr, cr+1, cc , word ,idx+1) ||
-                        isExist(arr, cr-1, cc , word ,idx+1) ||
-                        isExist(arr, cr, cc+1 , word ,idx+1) ||
-                        isExist(arr, cr, cc-1 , word ,idx+1);
-            arr[cr][cc] = word.charAt(idx);
-            return found;
 
+                         arr[cr][cc] = '&';
+        boolean found =  exit(arr,  cr+1 , cc , word, i+1) ||
+                         exit(arr,  cr , cc+1 , word, i+1) ||
+                         exit(arr,  cr-1 , cc , word, i+1) ||
+                         exit(arr,  cr , cc-1 , word, i+1) ;
+                         arr[cr][cc] = word.charAt(i);
+                         return found;
 
     }
 }
