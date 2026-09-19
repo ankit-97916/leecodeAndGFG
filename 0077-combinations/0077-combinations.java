2be66c952444774735373ee05bdac693965f50b4
Ankit Kumar <kumarankittdl31@gmail.com>
2026-09-19 20:31:45 +0530

    Time: 21 ms (24.96%), Space: 97.9 MB (65.53%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -2,22 +2,22 @@ class Solution {
     public List<List<Integer>> combine(int n, int k) {
          List<List<Integer>> ans = new ArrayList<>();
          List<Integer> li = new ArrayList<>();
-         combination(n , 1, k, 0, ans, li);
+         combination(n , 1, k, ans, li);
          return ans;
     }
 
-    public static void combination(int n, int i , int k , int currk,List<List<Integer>> ans, List<Integer> li){
-        if(currk == k){
+    public static void combination(int n, int i , int k ,List<List<Integer>> ans, List<Integer> li){
+        if(li.size() == k){
             ans.add(new ArrayList<>(li));
             return;
         }
-        if(currk > k){
+        if(li.size() > k){
             return;
         }
         //
         for(int j=i; j<=n; j++){
             li.add(j);
-            combination(n, j+1, k, currk+1, ans, li);
+            combination(n, j+1, k, ans, li);
             li.remove(li.size()-1);
         }
     }
